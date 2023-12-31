6ce854c2f81bb15c16c8460352f7e142b4620734
Kristiāns Kokars <56245893+KristiansKokars@users.noreply.github.com>
2023-12-31 16:12:19 +0200

    fix: Made network timeout more generous

--- app/src/main/java/com/kristianskokars/simplecatapp/core/Constants.kt
+++ app/src/main/java/com/kristianskokars/simplecatapp/core/Constants.kt
@@ -3,7 +3,7 @@ package com.kristianskokars.simplecatapp.core
 const val BASE_URL = "https://api.thecatapi.com/v1/"
 const val CAT_DATABASE = "cat_database"
 const val CAT_TABLE_NAME = "cats"
-const val NETWORK_TIMEOUT = 5L
+const val NETWORK_TIMEOUT = 25L
 const val CHANNEL_NAME = "Cat Nexus"
 const val CHANNEL_DESCRIPTION = "Cat Nexus Downloading Picture Notifications"
 const val CHANNEL_ID = "CatsNexus"
