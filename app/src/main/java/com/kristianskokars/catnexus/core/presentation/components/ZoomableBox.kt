a0d7202495a96d78dd54403b34defe10e7999ad4
KristiansKokars <kristians_kokars@outlook.com>
2024-02-13 00:15:43 +0200

    feat: added double tap to zoom back out

--- app/src/main/java/com/kristianskokars/catnexus/core/presentation/components/ZoomableBox.kt
+++ app/src/main/java/com/kristianskokars/catnexus/core/presentation/components/ZoomableBox.kt
@@ -1,13 +1,16 @@
 package com.kristianskokars.catnexus.core.presentation.components
 
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -15,6 +18,7 @@ import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.unit.IntSize
+import kotlinx.coroutines.launch
 
 @Composable
 fun ZoomableBox(
@@ -24,10 +28,15 @@ fun ZoomableBox(
     onScaleChange: (Float) -> Unit,
     content: @Composable ZoomableBoxScope.() -> Unit
 ) {
-    var scale by remember { mutableFloatStateOf(1f) }
-    var offsetX by remember { mutableFloatStateOf(0f) }
-    var offsetY by remember { mutableFloatStateOf(0f) }
+    val scale = remember { Animatable(1f) }
+    val offsetX = remember { Animatable(0f) }
+    val offsetY = remember { Animatable(0f) }
     var size by remember { mutableStateOf(IntSize.Zero) }
+    val coroutineScope = rememberCoroutineScope()
+
+    LaunchedEffect(key1 = scale.value) {
+        onScaleChange(scale.value)
+    }
 
     Box(
         modifier = modifier
@@ -35,18 +44,45 @@ fun ZoomableBox(
             .onSizeChanged { size = it }
             .pointerInput(Unit) {
                 detectTransformGestures { _, pan, zoom, _ ->
-                    scale = maxOf(minScale, minOf(scale * zoom, maxScale))
-                    onScaleChange(scale)
-                    val maxX = (size.width * (scale - 1)) / 2
+                    coroutineScope.launch {
+                        scale.animateTo(maxOf(minScale, minOf(scale.targetValue * zoom, maxScale)))
+                    }
+                    val maxX = (size.width * (scale.targetValue - 1)) / 2
                     val minX = -maxX
-                    offsetX = maxOf(minX, minOf(maxX, offsetX + pan.x))
-                    val maxY = (size.height * (scale - 1)) / 4
+                    coroutineScope.launch {
+                        offsetX.animateTo(
+                            maxOf(
+                                minX,
+                                minOf(maxX, offsetX.targetValue + pan.x)
+                            ),
+                            tween(0)
+                        )
+                    }
+                    val maxY = (size.height * (scale.targetValue - 1)) / 4
                     val minY = -maxY
-                    offsetY = maxOf(minY, minOf(maxY, offsetY + pan.y))
+                    coroutineScope.launch {
+                        offsetY.animateTo(
+                            maxOf(
+                                minY,
+                                minOf(maxY, offsetY.targetValue + pan.y)
+                            ),
+                            tween(0)
+                        )
+                    }
                 }
             }
     ) {
-        val scope = ZoomableBoxScopeImpl(scale, offsetX, offsetY, this)
+        val scope = ZoomableBoxScopeImpl(
+            scale.value,
+            offsetX.value,
+            offsetY.value,
+            zoomToDefault = {
+                coroutineScope.launch { scale.animateTo(1f, tween(300)) }
+                coroutineScope.launch { offsetX.animateTo(0f, tween(300)) }
+                coroutineScope.launch { offsetY.animateTo(0f, tween(300)) }
+            },
+            this
+        )
         scope.content()
     }
 }
@@ -55,11 +91,13 @@ interface ZoomableBoxScope : BoxScope {
     val scale: Float
     val offsetX: Float
     val offsetY: Float
+    val zoomToDefault: () -> Unit
 }
 
 private data class ZoomableBoxScopeImpl(
     override val scale: Float,
     override val offsetX: Float,
     override val offsetY: Float,
+    override val zoomToDefault: () -> Unit,
     val boxScope: BoxScope,
 ) : ZoomableBoxScope, BoxScope by boxScope
--- app/src/main/java/com/kristianskokars/catnexus/feature/cat_detail/presentation/CatDetailsScreen.kt
+++ app/src/main/java/com/kristianskokars/catnexus/feature/cat_detail/presentation/CatDetailsScreen.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -29,6 +30,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -154,6 +156,9 @@ fun CatDetailsContent(
                         .crossfade(true)
                         .build(),
                     modifier = Modifier
+                        .pointerInput(null) {
+                            detectTapGestures(onDoubleTap = { zoomToDefault() })
+                        }
                         .graphicsLayer(
                             scaleX = scale,
                             scaleY = scale,
