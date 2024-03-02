6aa91a346e76812747da90307a31f681ba66a02f
Kristiāns Kokars <56245893+KristiansKokars@users.noreply.github.com>
2024-03-02 17:40:26 +0200

    feat: added in-app notification for a cat finishing download (#53)

---
+++ app/src/main/java/com/kristianskokars/catnexus/core/presentation/components/Toast.kt
@@ -0,0 +1,74 @@
+package com.kristianskokars.catnexus.core.presentation.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.SnackbarData
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.kristianskokars.catnexus.R
+import com.kristianskokars.catnexus.core.presentation.theme.Black
+
+@Composable
+fun BelowTopBarDownloadToast(
+    modifier: Modifier = Modifier,
+    hostState: SnackbarHostState
+) {
+    SnackbarHost(hostState = hostState) { data ->
+        Box(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(top = 124.dp)
+        ) {
+            Toast(
+                modifier = Modifier.align(Alignment.TopCenter),
+                data = data,
+            )
+        }
+    }
+}
+
+@Composable
+private fun Toast(
+    modifier: Modifier = Modifier,
+    data: SnackbarData,
+) {
+    Row(
+        modifier = modifier
+            .padding(vertical = 24.dp, horizontal = 36.dp)
+            .background(Black, CircleShape)
+            .border(
+                Dp.Hairline,
+                Color.Gray.copy(alpha = 0.75f),
+                CircleShape
+            )
+            .padding(8.dp),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Icon(
+            modifier = Modifier.size(24.dp),
+            painter = painterResource(id = R.drawable.ic_download),
+            contentDescription = null,
+            tint = Color.White,
+        )
+        Spacer(modifier = Modifier.size(6.dp))
+        Text(text = data.visuals.message)
+    }
+}
