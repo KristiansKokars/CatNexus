ccc7caebb42e0b680576bf68c0068300e3a02b16
Kristiāns Kokars <56245893+KristiansKokars@users.noreply.github.com>
2024-03-02 14:31:35 +0200

    feat: added custom splash screen (#51)

--- app/src/main/java/com/kristianskokars/catnexus/MainActivity.kt
+++ app/src/main/java/com/kristianskokars/catnexus/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import coil.ImageLoader
 import com.google.accompanist.navigation.material.ExperimentalMaterialNavigationApi
 import com.kristianskokars.catnexus.core.presentation.components.BackgroundSurface
@@ -24,7 +25,9 @@ class MainActivity : ComponentActivity() {
 
     @OptIn(ExperimentalMaterialNavigationApi::class, ExperimentalAnimationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge(
             statusBarStyle = SystemBarStyle.dark(Color.TRANSPARENT),
             navigationBarStyle = SystemBarStyle.dark(resources.getColor(R.color.black, null))
