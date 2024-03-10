3621676d710f94be4619ef83da17aecbeb9c1afe
KristiansKokars <kristians_kokars@outlook.com>
2024-03-10 14:49:30 +0200

    fix: added more defined borders around UI elements

--- app/src/main/java/com/kristianskokars/catnexus/core/presentation/DefaultHazeStyle.kt
+++ app/src/main/java/com/kristianskokars/catnexus/core/presentation/DefaultHazeStyle.kt
@@ -2,6 +2,8 @@ package com.kristianskokars.catnexus.core.presentation
 
 import androidx.compose.ui.unit.dp
 import com.kristianskokars.catnexus.core.presentation.theme.Black
+import com.kristianskokars.catnexus.core.presentation.theme.Gray900
 import dev.chrisbanes.haze.HazeStyle
 
 val DefaultHazeStyle = HazeStyle(tint = Black.copy(alpha = 0.72f), blurRadius = 24.dp)
+val ElevatedHazeStyle = HazeStyle(tint = Gray900.copy(alpha = 0.72f), blurRadius = 24.dp)
