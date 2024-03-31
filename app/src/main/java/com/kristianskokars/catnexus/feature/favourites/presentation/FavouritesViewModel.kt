ff1f1a8af4de4d9765daecd5d0524a23650cd26c
KristiansKokars <kristians_kokars@outlook.com>
2024-03-31 22:10:36 +0300

    fix: fixed easter egg loading in bug

--- app/src/main/java/com/kristianskokars/catnexus/feature/favourites/presentation/FavouritesViewModel.kt
+++ app/src/main/java/com/kristianskokars/catnexus/feature/favourites/presentation/FavouritesViewModel.kt
@@ -1,10 +1,8 @@
 package com.kristianskokars.catnexus.feature.favourites.presentation
 
-import androidx.datastore.core.DataStore
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.kristianskokars.catnexus.core.domain.CarModeListener
-import com.kristianskokars.catnexus.core.domain.model.UserSettings
+import com.kristianskokars.catnexus.core.domain.CarModeHandler
 import com.kristianskokars.catnexus.core.domain.repository.CatRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.SharingStarted
@@ -15,21 +13,18 @@ import javax.inject.Inject
 
 @HiltViewModel
 class FavouritesViewModel @Inject constructor(
-    private val carModeListener: CarModeListener,
+    private val carModeHandler: CarModeHandler,
     repository: CatRepository,
-    userSettingsStore: DataStore<UserSettings>,
 ) : ViewModel() {
     val state = repository.getFavouritedCats()
         .map { FavouritesState(cats = it) }
         .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), FavouritesState())
 
-    val isInCarMode = userSettingsStore.data
-        .map { it.isInCarMode }
-        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), false)
+    val isInCarMode = carModeHandler.isInCarMode
 
     fun onCatNexusLogoClick() {
         viewModelScope.launch {
-            carModeListener.onCatNexusLogoClick()
+            carModeHandler.onCatNexusLogoClick()
         }
     }
 }
