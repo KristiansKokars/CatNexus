63b80800edf240b27edf5363e4781710558ff9e2
KristiansKokars <kristians_kokars@outlook.com>
2024-09-09 13:51:19 +0300

    chore: bumped app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.kristianskokars.catnexus"
         minSdk = 23
         targetSdk = 34
-        versionCode = 15
-        versionName = "1.3.1"
+        versionCode = 16
+        versionName = "1.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         //         testInstrumentationRunner = "com.kristianskokars.catnexus.app.HiltTestRunner"
