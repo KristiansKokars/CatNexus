7b442f7d04362ead735f71dbe13079707b3161c0
KristiansKokars <kristians_kokars@outlook.com>
2026-04-17 23:00:39 +0300

    chore(deps): bumped AGP version

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,8 @@ buildscript {
 }
 
 plugins {
-    id("com.android.application") version "8.12.0" apply false
-    id("com.android.library") version "8.12.0" apply false
+    id("com.android.application") version "8.13.2" apply false
+    id("com.android.library") version "8.13.2" apply false
     id("org.jetbrains.kotlin.android") version "2.2.0" apply false
     id("com.google.devtools.ksp") version "2.2.0-2.0.2" apply false
     id("io.sentry.android.gradle") version "5.8.1" apply false
